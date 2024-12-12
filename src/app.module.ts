2f986d6165d35ff2224f16deb50dd25fdf3cc232
Yoann Gendrey <ygendrey@gmail.com>
2024-12-12 22:25:08 +0100

    fix: format levels

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,30 @@ import { LoggerModule } from "nestjs-pino";
 import { appConfig } from "./app.config";
 import { ConnectionManagerService } from "./connection-manager.service";
 
+
+const levelStrings = {
+  10: 'debug',
+  20: 'info',
+  30: 'warn',
+  40: 'error',
+  50: 'fatal'
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate: (config: unknown) => appConfig.parse(config),
     }),
-    LoggerModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp:{
+        formatters:{
+            level(label, number) {
+                return { level: levelStrings[number] || label };
+            }
+        }
+      }
+    }),
   ],
   providers: [ConnectionManagerService],
 })
